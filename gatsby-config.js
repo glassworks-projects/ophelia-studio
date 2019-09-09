ad51f032af47115082f195029b3590d9d0b24ea4
glassworks-projects <ryan.michael.glassman@gmail.com>
2019-09-09 00:29:48 -0400

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ophelia Studio`,
+    description: `A design and digital studio based in Los Angeles, CA.`,
+    author: `@glassworks-projects`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,8 +25,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/ophelia-favicon-01.png`, // This path is relative to the root of the site.
+      },      
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
